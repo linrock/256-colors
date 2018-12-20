590dad2a17c69a587a75c555e94c7245a495115d
Linmiao Xu <linmiao.xu@gmail.com>
2018-12-20 17:04:25 +0900

    Display 256 color codes as squares

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,57 +1,54 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <div class="s-8x2">
+      <square v-for="i in 16" :key="i-1" :code="i-1"/>
+    </div>
+
+    <div class="container-6x6">
+      <div v-for="i in 6" :key="`sq-${i}`" class="s-6x6">
+        <square v-for="j in 36" :key="16 + (i-1)*36 + (j-1)" :code="16 + (i-1)*36 + (j-1)"/>
+      </div>
+    </div>
+
+    <div class="s-8x2">
+      <square v-for="i in 24" :key="231 + i" :code="231 + i"/>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  import Square from './square'
+
+  export default {
+    name: 'HelloWorld',
+    components: {
+      Square
+    }
   }
-}
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="stylus">
-h3
-  margin 40px 0 0
+<style lang="stylus" scoped>
+  square-size = 24px
 
-ul
-  list-style-type none
-  padding 0
+  .s-8x2
+    width square-size * 8
+    height square-size *  2
+    display flex
+    flex-wrap wrap
 
-li
-  display inline-block
-  margin 0 10px
+  .s-6x6
+    width square-size * 6
+    height square-size * 6
+    display flex
+    flex-wrap wrap
 
-a
-  color #42b983
+  .container-6x6
+    width square-size * 6 * 3
+    display flex
+    flex-wrap wrap
+
+  .square
+    width 24px
+    height 24px
+
 </style>
---
+++ src/components/square.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="square" :style="[{ background: COLORS[code] }]"></div>
+</template>
+
+<script>
+  import COLORS from '../utils/colors'
+
+  export default {
+    props: {
+      code: {
+        type: Number,
+        required: true
+      },
+    },
+    data() {
+      return {
+        COLORS
+      }
+    }
+  }
+</script>
